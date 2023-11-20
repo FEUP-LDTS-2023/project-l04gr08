a012b67d8b38a9aa75bf2b1b9962a16454bfcf6d
Sofia <sofiaagoncalves25@gmail.com>
2023-11-20 17:07:43 +0000

    Creation of Map, Wall and Key

---
+++ projectST/src/main/java/com/st/projectst/model/Key.java
@@ -0,0 +1,14 @@
+package com.st.projectst.model;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Key extends GameObject{
+    public Key(Position position) {
+        super(position);
+    }
+
+    @Override
+    public void draw(TextGraphics graphics) {
+
+    }
+}
---
+++ projectST/src/main/java/com/st/projectst/model/MapBuilder.java
@@ -0,0 +1,76 @@
+package com.st.projectst.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapBuilder {
+    private List<String> linesMap;
+    private int width;
+    private int height;
+
+    public Map buildMap(String filePath, int level) throws IOException {
+        linesMap = loadFromFile(filePath);
+        width = linesMap.get(0).length();
+        height = linesMap.size();
+
+        Map map = new Map(width, height, level);
+        map.setMari(createMari());
+        map.setEnemies(createEnemies());
+        map.setWalls(createWalls());
+        map.setKey(createKey());
+        return map;
+    }
+
+    public List<String> loadFromFile(String filePath) throws IOException {
+        List<String> lines = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+        for (String line; (line = reader.readLine()) != null; )
+            lines.add(line);
+
+        return lines;
+    }
+
+    private Hero createMari() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (linesMap.get(y).charAt(x) == 'M')
+                    return new Hero(new Position(x, y));
+            }
+        }
+        return null;
+    }
+    private List<Enemy> createEnemies() {
+        List<Enemy> enemies = new ArrayList<>();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (linesMap.get(y).charAt(x) == 'G')
+                    enemies.add(new GhostEnemy(new Position(x, y)));
+            }
+        }
+        return enemies;
+    }
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (linesMap.get(y).charAt(x) == 'W')
+                    walls.add(new Wall(new Position(x,y)));
+            }
+        }
+        return walls;
+    }
+    private Key createKey() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (linesMap.get(y).charAt(x) == 'K')
+                    return new Key(new Position(x, y));
+            }
+        }
+        return null;
+    }
+
+}
