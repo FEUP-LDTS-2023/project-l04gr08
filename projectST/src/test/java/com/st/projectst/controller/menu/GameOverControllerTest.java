d779c0c63d7d784403aa895a18f71c4c4e52337f
teresaam7 <teresamas2010@gmail.com>
2023-12-08 18:36:11 +0000

    Tests for the controllers' classes related to menu

---
+++ projectST/src/test/java/com/st/projectst/controller/menu/GameOverControllerTest.java
@@ -0,0 +1,61 @@
+package com.st.projectst.controller.menu;
+
+import com.st.projectst.Main;
+import com.st.projectst.gui.GUI;
+import com.st.projectst.gui.LanternaGUI;
+import com.st.projectst.model.menu.GameOver;
+import com.st.projectst.states.StartState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.awt.FontFormatException;
+
+import static org.mockito.Mockito.*;
+
+public class GameOverControllerTest {
+
+    private GameOver gameOver;
+    private GameOverController gameOverController;
+    private Main main;
+
+    @BeforeEach
+    public void setUp() {
+        gameOver = mock(GameOver.class);
+        gameOverController = new GameOverController(gameOver);
+        main = mock(Main.class);
+        when(main.getGui()).thenReturn(mock(LanternaGUI.class));
+    }
+
+    @Test
+    public void testStepUp() throws IOException, URISyntaxException, FontFormatException {
+        gameOverController.step(main, GUI.ACTION.UP, 1000);
+        verify(gameOver, times(1)).previousOption();
+    }
+
+    @Test
+    public void testStepDown() throws IOException, URISyntaxException, FontFormatException {
+        gameOverController.step(main, GUI.ACTION.DOWN, 1000);
+        verify(gameOver, times(1)).nextOption();
+    }
+
+    @Test
+    public void testStepSelectGoBack() throws IOException, URISyntaxException, FontFormatException {
+        when(gameOver.isSelectedGoBack()).thenReturn(true);
+        gameOverController.step(main, GUI.ACTION.SELECT, 1000);
+        verify(main.getGui(), times(1)).close();
+        verify(main).setGui(any(LanternaGUI.class));
+        verify(main).setState(any(StartState.class));
+    }
+
+    @Test
+    public void testStepSelectNotGoBack() throws IOException, URISyntaxException, FontFormatException {
+        when(gameOver.isSelectedGoBack()).thenReturn(false);
+        gameOverController.step(main, GUI.ACTION.SELECT, 1000);
+        verify(main.getGui(), never()).close();
+        verify(main, never()).setGui(any(LanternaGUI.class));
+        verify(main, never()).setState(any(StartState.class));
+    }
+}
+
